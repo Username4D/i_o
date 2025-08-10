b4f8b6d85375664dc8d50ad123b5e2a62c240019
Username4D <studio@kremzow.org>
2025-08-10 20:46:25 +0200

    Release: 1.4

--- global_view.gd
+++ global_view.gd
@@ -1,13 +1,13 @@
 extends Node2D
 
 func start_black():
-	for i in range(0.0, 21.0):
-		$ColorRect.modulate.a = i / 20.0
+	for i in range(0.0, 41.0):
+		$ColorRect.modulate.a = i / 40.0
 		await get_tree().process_frame
 	ui_handler.black_screen.emit()
 	await get_tree().process_frame
-	for i in range(0.0, 21.0):
-		$ColorRect.modulate.a =1.0 -  i / 20.0
+	for i in range(0.0, 41.0):
+		$ColorRect.modulate.a =1.0 -  i / 40.0
 		await get_tree().process_frame
 func _ready() -> void:
 	print(OS.is_userfs_persistent())
--- loader.gd
+++ loader.gd
@@ -5,10 +5,11 @@ func _process(delta: float) -> void:
 		$Sprite2D.modulate.a = 1.0 - ($Timer.time_left - 1.5)
 	elif $Timer.time_left < 1.0:
 		$Sprite2D.modulate.a =$Timer.time_left
-	elif $Timer.time_left > 0.0:
+	elif $Timer.time_left > 0.9:
 		$Sprite2D.modulate.a = 1.0
 	else:
 		$Sprite2D.modulate.a = 0.0
+
 
 
 func _on_timer_timeout() -> void:
